4224c4bf2f11d50401498348fe217682e9fdf5e3
Teema Khawjit <teema.khawjit@gmail.com>
2023-03-19 19:44:02 +0700

    Initial Commit

---
+++ Learn/LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnApp.swift
+//  Learn
+//
+//  Created by Teema Khawjit on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
