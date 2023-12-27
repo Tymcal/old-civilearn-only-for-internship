d045369638b0b27e5cc8ef3cbb03ae93bc5a794e
Tymcal <Teema.Khawjit@outlook.com>
2023-12-27 11:08:49 +0700

    Wed, Dec 27, 2023

--- Learn/ContentView.swift
+++ Learn/ContentView.swift
@@ -8,19 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isLoggedIn = false
+    @State private var token: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if isLoggedIn {
+            HomeView(isLoggedIn: $isLoggedIn, token: $token)
+        } else {
+            AuthView(isLoggedIn: $isLoggedIn, token: $token)
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
