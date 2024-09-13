404973ca71f4bfb2886911ee6603deea7006a89d
Tymcal <Teema.Khawjit@outlook.com>
2024-09-13 18:10:44 +0700

    Sep 13 2024

---
+++ Learn/Filler/PresentView.swift
@@ -0,0 +1,18 @@
+//
+//  PresentView.swift
+//  Learn
+//
+//  Created by Teema Khawjit on 9/4/24.
+//
+
+import SwiftUI
+
+struct PresentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PresentView()
+}
---
+++ Learn/User/testModel.swift
@@ -0,0 +1,8 @@
+//
+//  testModel.swift
+//  Learn
+//
+//  Created by Teema Khawjit on 9/5/24.
+//
+
+import Foundation
