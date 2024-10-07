4d431d4acecdf859894bbe594a5f8a04646cc632
Tymcal <Teema.Khawjit@outlook.com>
2024-10-07 10:49:00 +0700

    PresentView Done!

---
+++ FutureView.swift
@@ -0,0 +1,18 @@
+//
+//  FutureView.swift
+//  Learn
+//
+//  Created by Teema Khawjit on 9/27/24.
+//
+
+import SwiftUI
+
+struct FutureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FutureView()
+}
